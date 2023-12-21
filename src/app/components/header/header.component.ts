269c8e81874f97f872459f6408459ffd87fbde39
Sefat Anam <sefatanam@gmail.com>
2023-12-21 22:35:32 +0600

    fix: responsive nav issue

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, afterNextRender } from '@angular/core';
+import { AfterContentInit, Component, NgZone, afterNextRender, inject } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +8,15 @@ import { Component, afterNextRender } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterContentInit {
   openMenu: boolean = true;
-  constructor(){
-    afterNextRender(()=>{
-      this.openMenu = window.matchMedia('(max-width: 767px)').matches ? false : true;
+  ngZone = inject(NgZone)
+  
+  ngAfterContentInit(): void {
+    this.ngZone.runOutsideAngular(() => {
+      window.addEventListener('resize', () => {
+        this.openMenu = window.matchMedia('(max-width: 767px)').matches ? false : true;
+      })
     })
   }
 }

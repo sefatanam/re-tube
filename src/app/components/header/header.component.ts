d3acff48066147c5f07922a63691e484ceb35a6b
Sefat Anam <sefatanam@gmail.com>
2023-12-21 22:45:38 +0600

    dumb: header cleanup

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { AfterContentInit, Component, NgZone, afterNextRender, inject } from '@angular/core';
+import { Component, afterNextRender } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,15 +8,11 @@ import { AfterContentInit, Component, NgZone, afterNextRender, inject } from '@a
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent implements AfterContentInit {
+export class HeaderComponent {
   openMenu: boolean = true;
-  ngZone = inject(NgZone)
-  
-  ngAfterContentInit(): void {
-    this.ngZone.runOutsideAngular(() => {
-      window.addEventListener('resize', () => {
-        this.openMenu = window.matchMedia('(max-width: 767px)').matches ? false : true;
-      })
+  constructor() {
+    afterNextRender(() => {
+      this.openMenu = window.matchMedia('(max-width: 767px)').matches ? false : true;
     })
   }
 }

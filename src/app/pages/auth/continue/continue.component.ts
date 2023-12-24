7e28854145a7865c7dd9c65418c6dfabc12b3131
Sefat Anam <sefatanam@gmail.com>
2023-12-24 21:48:30 +0600

    feat(login): continue with google init

--- src/app/pages/auth/continue/continue.component.ts
+++ src/app/pages/auth/continue/continue.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from 'services/auth.service';
+import { ButtonComponent } from "../../../components/button/button.component";
 
 @Component({
-  selector: 'app-continue',
-  standalone: true,
-  imports: [],
-  templateUrl: './continue.component.html',
-  styleUrl: './continue.component.scss'
+    selector: 'app-continue',
+    standalone: true,
+    templateUrl: './continue.component.html',
+    styleUrl: './continue.component.scss',
+    imports: [ButtonComponent]
 })
 export class ContinueComponent {
+
+  authService = inject(AuthService);
 
 }

44592096bc219d3908f0d6ce2e78dd7ae846cfa6
Sefat Anam <sefatanam@gmail.com>
2023-12-30 00:38:58 +0600

    feat(player): sticky player for mobile device

--- src/app/components/mobile-menus/mobile-menus.component.ts
+++ src/app/components/mobile-menus/mobile-menus.component.ts
@@ -1,17 +1,24 @@
-import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RootMenu } from '@typings/menu.type';
 import { ButtonComponent } from "../button/button.component";
 import { User } from 'firebase/auth';
+import { AuthService } from 'services/auth.service';
 @Component({
-    selector: 'app-mobile-menus',
-    standalone: true,
-    templateUrl: './mobile-menus.component.html',
-    styleUrl: './mobile-menus.component.scss',
-    imports:  [RouterLink, ButtonComponent]
+  selector: 'app-mobile-menus',
+  standalone: true,
+  templateUrl: './mobile-menus.component.html',
+  styleUrl: './mobile-menus.component.scss',
+  imports: [RouterLink, ButtonComponent]
 })
 export class MobileMenusComponent {
+  authService = inject(AuthService)
+
   @Input() user = signal<User | null>(null);
-  @Input() menus: RootMenu = {routeButtons:[], routeMenus:[]}
+  @Input() menus: RootMenu = { routeButtons: [], routeMenus: [] }
   @Output() onMenuItemClick = new EventEmitter<void>()
+
+
+  kickOut = () => this.authService.signOut();
+
 }

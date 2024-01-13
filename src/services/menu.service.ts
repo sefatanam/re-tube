14797aaf9817a0f4c7429a438cb39744b01a08f8
Sefat Anam <sefatanam@gmail.com>
2024-01-13 23:20:09 +0600

    refactor(menus): update menu props

--- src/services/menu.service.ts
+++ src/services/menu.service.ts
@@ -1,41 +1,60 @@
-import { Injectable } from '@angular/core';
-import { Menu, RootMenu } from '@typings/menu.type';
+import {inject, Injectable} from '@angular/core';
+import {Menu} from '@typings/menu.type';
+import {AuthService} from "@services/auth.service";
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
+  authService = inject(AuthService);
 
-  private routeMenus: Menu[] = [
-    {
-      name: 'What is Re-tube?',
-      route: '/'
-    },
-    {
-      name: 'Player',
-      route: '/player'
-    },
-    {
-      name: 'Community',
-      route: '#'
-    },
-    {
-      name: 'Showcase',
-      route: '#'
-    }
-  ]
+  private routeButtons: Menu[] = this.makeRouteButtons();
+  private routeMenus: Menu[] = this.makeRouteMenus();
 
-
-  private routeButtons: Menu[] = [
-    {
-      name: 'Login',
-      route: '/login'
-    }
-  ]
-
-  rootMenu: RootMenu = {
+  public rootMenu = ({
     routeButtons: this.routeButtons,
     routeMenus: this.routeMenus
+  })
+
+  public updateRoutesPermission() {
+    return this.rootMenu = ({
+      routeButtons: this.makeRouteButtons(),
+      routeMenus: this.makeRouteMenus()
+    })
+  }
+
+  private makeRouteMenus(): Menu[] {
+    return [
+      {
+        name: 'What is Re-tube?',
+        route: '/',
+        isAvailable: !this.authService.authUser()
+      },
+      {
+        name: 'Public',
+        route: '/player',
+        isAvailable: true
+      },
+      // TODO: will be used from here in future
+      // {
+      //   name: 'Personal',
+      //   route: '/player',
+      //   isAvailable: !!this.authService.authUser()?.uid,
+      //   queryParams: {
+      //     "uid": this.authService.authUser()?.uid ?? 'n/a'
+      //   }
+      // }
+    ]
+  }
+
+  private makeRouteButtons(): Menu[] {
+    return [
+      {
+        name: 'Login',
+        route: '/login',
+        isAvailable: true
+      }
+    ]
   }
 }

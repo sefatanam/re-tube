8e1d78347413c256ebd9533f6537e74b675608fb
Sefat Anam <sefatanam@gmail.com>
2024-01-31 23:33:26 +0600

    revert youtube service

--- src/services/youtube.service.ts
+++ src/services/youtube.service.ts
@@ -3,48 +3,44 @@ import { collection, collectionData, doc, Firestore, setDoc, query, where, onSna
 import { VideoInfo } from "@interface/video-info.interface";
 import { from, Observable, switchMap, tap } from "rxjs";
 import { YoutubeUtil } from "@utils/youtube.util";
+import { db } from "../indexDB/db";
 import firebase from "firebase/compat";
 import Unsubscribe = firebase.Unsubscribe;
-import { DexieService } from './dexie.service';
 
 const COLLECTION_NAME = 'videos';
-const DEXIE_TABLE = 'videos';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
-
   firestore: Firestore = inject(Firestore);
   youtubeUtil = inject(YoutubeUtil);
-  dexieService = inject(DexieService);
   private destroyVideosSnapshot !: Unsubscribe;
 
   async videosRealtimeUpdateInit() {
     const q = query(collection(this.firestore, COLLECTION_NAME));
     onSnapshot(q, async (snapshot) => {
-      if (snapshot.docs.length > await this.dexieService.countItems('videos')) {
-        this.dexieService.clearTable('video')
+      if (snapshot.docs.length > await this.countVideos()) {
+        this.clearCache();
         this.getVideos();
       }
     });
   }
 
   getVideos(): Observable<VideoInfo[]> {
-    return from(this.dexieService.countItems(DEXIE_TABLE)).pipe(
+    return from(this.countVideos()).pipe(
       switchMap((count) => {
         if (count > 0) {
-          return from(this.dexieService.getItems<VideoInfo>(DEXIE_TABLE));
+          return from(this.getCacheVideos());
         } else {
           const aCollection = collection(this.firestore, COLLECTION_NAME);
           return collectionData(aCollection) as Observable<VideoInfo[]>;
         }
       }),
       tap(async (videos) => {
-        await this.dexieService.clearTable(DEXIE_TABLE);
+        await this.clearCache();
         if (Array.isArray(videos)) {
-          await this.dexieService.blukAdd(DEXIE_TABLE, videos);
+          await this.addVideosToCache(videos);
         }
       })
     );
@@ -57,7 +53,31 @@ export class YoutubeService {
   async saveVideo(videoInfo: VideoInfo) {
     const docId = this.youtubeUtil.generateVideoInfoId(videoInfo);
     await setDoc(doc(this.firestore, COLLECTION_NAME, docId), videoInfo)
-      .finally(async () => await this.dexieService.clearTable(DEXIE_TABLE));
   }
 
+  private async countVideos() {
+    return db.open().then(() => {
+      return db.videos.count();
+    })
+
+  }
+
+  private async getCacheVideos() {
+    return db.open().then(() => {
+      return db.videos.toArray();
+    })
+
+  }
+
+  private async addVideosToCache(videos: VideoInfo[]) {
+    return db.open().then(async () => {
+      await db.videos.bulkAdd(videos);
+    })
+  }
+
+  private async clearCache() {
+    return db.open().then(async () => {
+      await db.videos.clear()
+    })
+  }
 }

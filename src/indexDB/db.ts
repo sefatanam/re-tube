70fb17ac2eec643053ff0c060c9109326736cbd8
Sefat Anam <sefatanam@gmail.com>
2024-01-31 16:08:22 +0600

    refactor(youtube-service): remove all dexie logic to dexie service

--- src/indexDB/db.ts
+++ src/indexDB/db.ts
@@ -1,20 +1,23 @@
 import Dexie, { Table } from 'dexie';
-import {VideoInfo} from "@interface/video-info.interface";
+import { VideoInfo } from "@interface/video-info.interface";
 export class AppDB extends Dexie {
   videos!: Table<VideoInfo, "title">;
   constructor() {
     super('ReTube');
     this.version(1).stores({
-      videos:'title'
+      videos: 'title'
     })
 
-    this.open().then(()=>console.log('Opened')).catch((err)=>console.error(err)).finally(()=>{
-      if(this.isOpen()){
-        console.log('Connected to IndexDB')
-      }else{
-        console.log('IndexDB instance error')
-      }
-    })
+    this.open()
+      .then(() => console.log('Opened'))
+      .catch((err) => console.error(err))
+      .finally(() => {
+        if (this.isOpen()) {
+          console.log('Connected to IndexDB');
+        } else {
+          console.log('IndexDB instance error');
+        }
+      });
   }
 }
-export const db= new AppDB()
+export const db = new AppDB()

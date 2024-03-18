481f4060da2260be11d6e96aea038f62f990e5f2
Sefat Anam <sefatanam@gmail.com>
2024-03-18 23:47:18 +0600

    feat: add gsap text animation in homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { environment } from 'environments/environment.dev';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
+export class HomeComponent implements AfterViewInit {
   env = environment;
+  /* heading
+paragraph */
 
+  ngAfterViewInit(): void {
+    gsap.to('.heading', {
+      ease: 'power1.in',
+      opacity: 1,
+      y: 0,
+    });
+
+    gsap.fromTo(
+      '.paragraph',
+      {
+        opacity: 0,
+        y: 10,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: 1,
+        stagger: 0.1,
+      }
+    );
+  }
 }

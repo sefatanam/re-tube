0491fa42b6b7517880af504a18fea8c9b7c14eab
Sefat Anam <sefatanam@gmail.com>
2024-03-23 15:58:03 +0600

    feat: remove karma and add jest for testing

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 're-tube',
+  preset: './jest.preset.js',
+  coverageDirectory: './coverage/re-tube',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
